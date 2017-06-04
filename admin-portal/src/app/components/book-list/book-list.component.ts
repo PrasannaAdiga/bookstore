9e570606585848f9566535cef24abbcf73436199
Prasanna Adiga <gpadiga.adiga@gmail.com>
2017-06-04 18:35:01 +0530

    implementing delete all/selected books functionality

--- admin-portal/src/app/components/book-list/book-list.component.ts
+++ admin-portal/src/app/components/book-list/book-list.component.ts
@@ -51,6 +51,45 @@ export class BookListComponent implements OnInit {
       );
   }
 
+  updateRemoveBookList(checked: boolean, book: Book) {
+    if (checked) {
+      this.removeBookList.push(book);
+    } else {
+      this.removeBookList.splice(this.removeBookList.indexOf(book), 1);
+    }
+  }
+
+  updateSelected(checked: boolean) {
+    if (checked) {
+      this.allChecked = true;
+      this.removeBookList = this.bookList.slice();
+    } else {
+      this.allChecked = false;
+      this.removeBookList = [];
+    }
+  }
+
+  removeSelectedBooks() {
+    const dialogRef = this.dialog.open(DialogResultExampleDialog);
+    dialogRef.afterClosed().subscribe(
+      result => {
+        console.log(result);
+        if (result === 'yes') {
+          for (const book of this.removeBookList) {
+            this.removeBookService.sendBook(book.id).subscribe(
+              res => {
+
+              },
+              err => {
+              }
+              );
+          }
+          location.reload();
+        }
+      }
+      );
+  }
+
   getBookList() {
     this.getBookListService.getBookList().subscribe(
       res => {
